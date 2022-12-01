12da3f856198eeaf1cba24f2790eba44fe1f1672
bitnap1 <119562446+bitnap1@users.noreply.github.com>
2022-12-01 22:30:41 +0900

    added BirdJump script

--- birdy2/Assets/BirdJump.cs
+++ birdy2/Assets/BirdJump.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class BirdJump : MonoBehaviour
 {
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = Vector2.up * 3;            
+        }
     }
 }
