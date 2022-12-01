ab31db984807bf0f7d73bfc863b6b54b6a130ab0
bitnap1 <119562446+bitnap1@users.noreply.github.com>
2022-12-01 22:35:37 +0900

    changed constant into variable

--- birdy2/Assets/BirdJump.cs
+++ birdy2/Assets/BirdJump.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class BirdJump : MonoBehaviour
 {
     Rigidbody2D rb;
+
+    public float jumpPower;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +18,7 @@ public class BirdJump : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            rb.velocity = Vector2.up * 3;            
+            rb.velocity = Vector2.up * jumpPower;            
         }
     }
 }
