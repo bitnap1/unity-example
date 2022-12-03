dcee15c55dbb393cc8d23ac7ede76b5dc49e8e2f
bitnap1 <119562446+bitnap1@users.noreply.github.com>
2022-12-03 17:22:03 +0900

    created PipeGenerator

---
+++ birdy2/Assets/MakePipes.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakePipes : MonoBehaviour
+{
+    public GameObject pipe;
+    public float timeDiff;
+    float timer = 0;
+    public float rangeFrom;
+    public float rangeTo;    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > timeDiff)      // 1초가 지나면
+        {
+            GameObject newpipe = Instantiate(pipe);
+            newpipe.transform.position = new Vector3(6, Random.Range(-rangeFrom, rangeTo), 0);
+            timer = 0;
+        }
+        
+    }
+}
